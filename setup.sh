10099a397be1e88ab0d417b4cebe5612989a5cbd
megait004 <giapfc123@gmail.com>
2024-12-16 03:56:29 +0700

    Update setup script to streamline package installation and improve Chrome setup
    - Removed 'install' from the package installation command for clarity.
    - Added 'curl' to the list of installed packages.
    - Changed the installation command for Google Chrome from 'apt install' to 'dpkg -i' for better handling of the .deb file.
    - Cleaned up by removing the downloaded .deb file after installation.

--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-sudo apt install terminator neovim git zsh install build-essential openssh wget
+sudo apt install terminator neovim git zsh build-essential openssh wget curl
 
 echo 'deb http://download.opensuse.org/repositories/home:/lamlng/Debian_12/ /' | sudo tee /etc/apt/sources.list.d/home:lamlng.list
 curl -fsSL https://download.opensuse.org/repositories/home:lamlng/Debian_12/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_lamlng.gpg > /dev/null
 sudo apt update
 sudo apt install ibus-bamboo
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
